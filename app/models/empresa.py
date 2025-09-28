45f250f1f8a3ac9362a4c0e9e1a497e19782ef2e
Pedro Arthur <panrc100@gmail.com>
2025-09-27 23:40:14 -0300

    feat: Criar model empresa

---
+++ app/models/empresa.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timezone
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Empresa(Base):
+    __tablename__ = "empresas"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String, nullable=False)
+    cnpj = Column(String, unique=True, nullable=False)
+    cidade = Column(String, nullable=False)
+    ramo_atuacao = Column(String, nullable=False)
+    telefone = Column(String, nullable=False)
+    email_contato = Column(String, nullable=True)
+    contato = Column(String, nullable=True)
+    data_cadastro = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
