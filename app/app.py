f18781d5a2ab56f3734c4d7fc51f6c66046d6d40
Pedro Arthur <panrc100@gmail.com>
2025-09-29 02:50:03 -0300

    feat:Adicionar .gitginore

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from sqlalchemy.orm import Session
 
 from app.schemas.schemas import EmpresaCreate
 from app.models.empresa import Empresa
-from app.database import get_db
 
 app = FastAPI(title = "API cadastro de Empresas EcompJr")
 
@@ -14,10 +13,10 @@ def read_root():
     return {'message': 'Olá, mundo!'}
 
 @app.post('/empresas/')
-def create_firm(empresa: EmpresaCreate,  db: Session = Depends(get_db)):
+def create_firm(empresa: EmpresaCreate):
     db_empresa = Empresa(**empresa.model_dump())
 
-    db.add(db_empresa)
-    db.commit()
-    db.refresh(db_empresa)
+    #db.add(db_empresa)
+    #db.commit()
+    #db.refresh(db_empresa)
     return db_empresa
