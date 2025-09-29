13f2096633a845eab27a731e5daeacf7af076cae
Pedro Arthur <panrc100@gmail.com>
2025-09-28 22:50:05 -0300

    feat: Implementar rota POST

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 from app.schemas.schemas import EmpresaCreate
 from app.models.empresa import Empresa
+from app.database import get_db
 
 app = FastAPI(title = "API cadastro de Empresas EcompJr")
 
@@ -12,7 +14,7 @@ def read_root():
     return {'message': 'Olá, mundo!'}
 
 @app.post('/empresas/')
-def create_firm(empresa: EmpresaCreate):
+def create_firm(empresa: EmpresaCreate,  db: Session = Depends(get_db)):
     db_empresa = Empresa(**empresa.model_dump())
 
     db.add(db_empresa)
